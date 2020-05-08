2e6b7302fb20c0e1520a2df92fd895b92ae81604
Marc <51823734+MarcLefeuvre@users.noreply.github.com>
2020-05-08 18:49:39 +0200

    Piege

---
+++ Assets/Marc/Script/Piege.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Piege : MonoBehaviour
+{
+    public GameObject cube;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject)
+        {
+            cube.GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+}
+ 
